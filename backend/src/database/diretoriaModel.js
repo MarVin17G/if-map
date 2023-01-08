12c7ae163368b3bf8c47b0ef5105f384f5374f4f
Lucas Santos <lucassantos_dev@outlook.com>
2023-01-08 15:49:25 -0300

    fix model diretorias

--- backend/src/database/diretoriaModel.js
+++ backend/src/database/diretoriaModel.js
@@ -1,7 +1,7 @@
 const db = require('./connection');
 
 const create = async (nome, gestorPk) => {
-  const query = 'insert into ifmap.Diretorias (nome, gestorPk) values (?,?)';
+  const query = 'insert into ifmapdb.Diretorias (nome, gestorPk) values (?,?)';
   const [rows] = await db.execute(query, [nome, gestorPk]);
 
   return {
@@ -12,28 +12,30 @@ const create = async (nome, gestorPk) => {
 };
 
 const getAll = async () => {
-  const query = 'select * from ifmap.Diretorias';
+  const query = 'select * from ifmapdb.Diretorias';
   const [rows] = await db.execute(query);
   return rows;
 };
 
 const getById = async (diretoriaId) => {
-  const query = 'select * from ifmap.Diretorias where id = ?';
+  const query = 'select * from ifmapdb.Diretorias where diretoriaId = ?';
   const [rows] = await db.execute(query, [diretoriaId]);
   return rows[0];
 };
 
-const update = async (nome, diretoriaId) => {
-  const query = 'update ifmap.Diretorias set nome = ? where id = ?';
-  const [rows] = await db.execute(query, [nome, diretoriaId]);
+const update = async (nome, diretoriaId, gestorPk) => {
+  const query =
+    'update ifmapdb.Diretorias set nome = ?, set gestorPk = ? where diretoriaId = ?';
+  const [rows] = await db.execute(query, [nome, gestorPk, diretoriaId]);
 
   return {
     nome,
+    gestorPk,
   };
 };
 
 const remove = async (diretoriaId) => {
-  const query = 'delete from ifmap.Diretorias where id = ?';
+  const query = 'delete from ifmapdb.Diretorias where diretoriaId = ?';
   await db.execute(query, [diretoriaId]);
 };
 
