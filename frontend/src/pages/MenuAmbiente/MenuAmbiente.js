2796b5ef44215137eefdb2b1f46bcba4f6d6ec81
manrick <manricksantos@gmail.com>
2023-01-22 13:01:07 -0300

    Adicionando pagina de menus e pagina de guia por video

--- frontend/src/pages/MenuAmbiente/MenuAmbiente.js
+++ frontend/src/pages/MenuAmbiente/MenuAmbiente.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from "react";
-import Api from '../../sevices/Api';
 import { useSearchParams } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import Api from '../../sevices/Api';
+import './MenuAmbiente.css';
+
+import Header from './../../components/header/Header';
+import Footer from './../../components/footer/Footer';
+
+import { FaMapMarkedAlt } from 'react-icons/fa';
+import { AiOutlinePicture } from 'react-icons/ai';
+import { MdOutlineOndemandVideo } from 'react-icons/md';
 
 export default function MenuAmbiente () {
     const [ searchParams ] = useSearchParams();
@@ -19,7 +28,46 @@ export default function MenuAmbiente () {
 
     return (
        <>
-            
+            <Header />
+            <div className="div_menu_ambiente">
+                <Link to={`/guiaFoto?id=${ambiente.ambienteId}`}>
+                    <div className="cartao_dir cartao_dir_menu">
+                        <h3 className="title title_menu">Guia com foto</h3>
+                        <div class="bar bar_menu">
+                            <div class="emptybar empty_menu"></div>
+                            <div class="filledbar filledbar_menu"></div>
+                        </div>
+                        <div className="icon_menu">
+                            <AiOutlinePicture />
+                        </div>
+                    </div>
+                </Link>
+                <Link to={`/guiaVideo?id=${ambiente.ambienteId}`}>
+                    <div class="cartao_dir cartao_dir_menu">
+                        <h3 className="title title_menu">Guia com Video</h3>
+                        <div class="bar bar_menu">
+                            <div class="emptybar emptybar_menu"></div>
+                            <div class="filledbar filledbar_menu"></div>
+                        </div>
+                        <div className="icon_menu">
+                            <MdOutlineOndemandVideo />
+                        </div>
+                    </div>
+                </Link>
+                <Link to={`/guiaMapa?id=${ambiente.ambienteId}`}>
+                    <div className="cartao_dir cartao_dir_menu">
+                        <h3 class="title title_menu">Mapa</h3>
+                        <div class="bar bar_menu">
+                            <div class="emptybar emptybar_menu"></div>
+                            <div class="filledbar filledbar_menu"></div>
+                        </div>
+                        <div className="icon_menu">
+                            <FaMapMarkedAlt />
+                        </div>
+                    </div>
+                </Link>
+            </div>  
+            <Footer />
        </> 
     )
 }
