25a566f16ee39ee84b91ffe0fe9951f5db4f350c
manrick <manricksantos@gmail.com>
2023-04-13 08:50:14 -0300

    importando biblioteca do react

--- frontend/src/pages/Diretorias/index.jsx
+++ frontend/src/pages/Diretorias/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, React } from 'react';
 import Api from '../../services/api';
 
 import Header from '../../components/header';
